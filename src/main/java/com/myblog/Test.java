d78b3267793bff56ee8cf2ab4a149dba502d8fbb
SomaJoy <somajoy1083.sj@gmail.com>
2024-02-01 20:01:15 +0530

    101:Added spring security concept. 01/02/24

--- src/main/java/com/myblog/Test.java
+++ src/main/java/com/myblog/Test.java
@@ -6,8 +6,12 @@ import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
-        List<Integer> res = Arrays.asList(12, 20, 30, 40,43,57,23,43,21,24,76);
-        List<Integer> val = res.stream().sorted().collect(Collectors.toList());
-        System.out.println(val);
+        new Test().test().test1();
+    }
+    public Test test(){
+        return new Test();
+    }
+    public void test1(){
+        System.out.println(101);
     }
 }
